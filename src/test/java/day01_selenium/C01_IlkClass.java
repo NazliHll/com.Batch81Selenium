8df893c2d6d43aefd876bd601f541a06820055e4
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-27 16:53:07 +0300

    27.04.2023

---
+++ src/test/java/day01_selenium/C01_IlkClass.java
@@ -0,0 +1,24 @@
+package day01_selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_IlkClass {
+    public static void main(String[] args) throws InterruptedException {
+        /*En ilkel haliyle bir otomasyon yapmak için
+        classimiza otomasyon için gerekli olan webdriverin yerini göstermemiz gerekir
+        bunun için java kütüphanesinden System.Property() methodu kullaniriz.
+        ilki kullanacagimiz driver:webdriver.chrome.driver
+        ikincisi ise bu driverin fiziki yolu: */
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.twitter.com");
+        Thread.sleep(2000);
+        driver.get("https://www.facebook.com");
+
+        driver.close();
+    }
+
+
+}
