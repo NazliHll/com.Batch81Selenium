3e8140add6eae9f893c41604720577b96fb4971b
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-29 22:53:51 +0300

    29.04.2023

---
+++ src/test/java/day03_locators/C04_WebElementMethodlari.java
@@ -0,0 +1,49 @@
+package day03_locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C04_WebElementMethodlari {
+
+        /*
+        1- amazon.com a gidip arama kutusunu locate edin
+        2- arama kutusunun tagname inin input olduğunu test edin
+        3- arama kutusunun name attribute'nun değerinin field-keywords olduğunu test edin
+         */
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.amazon.com");
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+
+        String expectedTagName="input";
+        String actualTagName=aramaKutusu.getTagName();
+
+        if (expectedTagName.equals(actualTagName)){
+            System.out.println("tagname testi passed");
+        }else{
+            System.out.println("tagname testi failed");
+        }
+
+        String expectedNameDegeri="field-keywords";
+        String actualNameDegeri=aramaKutusu.getAttribute("name");
+
+        if (expectedNameDegeri.equals(actualNameDegeri)){
+            System.out.println("name attribute testi passed");
+        }else {
+            System.out.println("name attribute testi failed");
+
+        }
+        driver.close();
+
+
+
+    }
+}
