f148ee63b5603d3847687cb6feba301bc1214d2e
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-29 17:41:58 +0300

    29.04.2023

--- src/test/java/day02_driverMethodlari/C09_manageMethodlari.java
+++ src/test/java/day02_driverMethodlari/C09_manageMethodlari.java
@@ -10,6 +10,7 @@ public class C09_manageMethodlari {
         System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.amazon.com");
          /*
         Bir sayfa açılırken ilk başta sayfanın içerisinde bulunan
         elementlere göre bir yüklenme süresine ihtiyaç vardır.
@@ -17,6 +18,7 @@ public class C09_manageMethodlari {
         implicitlyWait bize sayfanın yüklenmesi ve sayfadaki elementlere ulaşım için
         beklenecek MAXIMUM sureyi belirleme olanağı tanır.
          */
+        driver.close();
 
     }
 }
