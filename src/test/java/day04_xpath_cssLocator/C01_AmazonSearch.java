eeaac7db22064a1945334eeb50cac71b1d08cc73
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-30 10:04:09 +0300

    30.04.2023

---
+++ src/test/java/day04_xpath_cssLocator/C01_AmazonSearch.java
@@ -0,0 +1,24 @@
+package day04_xpath_cssLocator;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_AmazonSearch {
+    /*
+        1-Main method oluşturun ve aşağıdaki görevi tamamlayın
+        a. https://amazon.com sayfasına gidin.
+        b. Search "city bike"
+        c. Amazonda görüntülenen ilgili sonuçların görüntülerini yazdırın
+        d. Sonra karşınıza çıkan ilk sonucun resmine tıklayın.
+         */
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.amazon.com");
+    }
+}
