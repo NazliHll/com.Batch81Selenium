90271448e89d230aa1280f44c452ab4e050c0dd1
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-30 10:30:07 +0300

    30.04.2023

--- src/test/java/day04_xpath_cssLocator/C01_AmazonSearch.java
+++ src/test/java/day04_xpath_cssLocator/C01_AmazonSearch.java
@@ -1,9 +1,13 @@
 package day04_xpath_cssLocator;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C01_AmazonSearch {
     /*
@@ -19,6 +23,16 @@ public class C01_AmazonSearch {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
-        driver.get("https://www.amazon.com");
+        driver.get("https://www.amazon.com/");
+        WebElement aramaKutusu= driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("city bike"+ Keys.ENTER);
+        List<WebElement> istenenElementListesi=driver.findElements(By.className("sg-col-inner"));
+        WebElement sonucYazisiElement=istenenElementListesi.get(0);
+        System.out.println(sonucYazisiElement.getText());
+
+        List<WebElement>urunResimleriList=driver.findElements(By.className("s-image"));
+        WebElement ilkUrunResmi=urunResimleriList.get(0);
+        ilkUrunResmi.click();
+
     }
 }
